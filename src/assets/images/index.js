59e7d5e75d5e57adbeac8c39bd22c7c6092452fb
Ankush Srivastava <ankushsri007@gmail.com>
2023-11-14 19:53:00 +0530

    commit done

---
+++ src/assets/images/index.js
@@ -0,0 +1,7 @@
+import hero from './hero.jpg'
+import logo from './logo.svg'
+
+export {
+    hero,
+    logo
+}
---
+++ src/constants/index.js
@@ -0,0 +1,219 @@
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: motion,
+        name: "Motion",
+        type: "Animation",
+    },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    }
+];
+
+export const experiences = [
+    {
+        title: "Software Engineer Virtual Exp",
+        company_name: "JP Morgan Chase&Co",
+        date: "Nov 2023-Nov 2023",
+        points: [
+            "Developing and maintaining web applications using core java and Web Technologies.",
+            "Learned and performed Unit testing and test Driven Development.",
+            "Setting up development Envoirment to local machine and performed bug fixex in code .",
+            "Learned and pracriced Software Engineering Pracitces .",
+        ],
+    },
+    {
+        title: "Walmart Tech Adv.Software Engg virtual Exp",
+        company_name: "Walmart",
+        date: "Jan 2023",
+        points: [
+            "Implemented Advanced Daata Strucures on an existing project",
+            "Designed an Implemented an RDBMS.",
+            "Performed Software Architecture Principles.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Software Engineerin Program",
+        company_name: "HP Enterpise",
+        date: "Feb 2023",
+        points: [
+            "Created Praposal for Resftul Web Services.",
+            "Built basic Restful Web Services.",
+            "Implemented SDLC and it's principles in Project.",
+            "Implented ability to uplad Data and written  unit tests.",
+        ],
+    },
+    {
+        title: "",
+        company_name: " Accenture Developer Program",
+        date: "May 2022",
+        points: [
+            "Defining Technichal Requirements.",
+            "Code Reviews and Debuging, Implemted SLDC.",
+            "Reading and Understanding code.",
+            "Understanding and Developing clound infrastrucutre.",
+        ],
+    },
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/Ankush-ai',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/ankush-srivastava-554931210/',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-red',
+        name: 'MedAppoint',
+        description: 'Developed a web application that helps patients to schedule their meetings with Doctors,Implemented Login feature for Dr and patient seperately.',
+        link: 'https://github.com/Ankush-ai/MedAppoint',
+    },
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: 'ChatHive',
+        description: 'Created a  Real time Discord Like Global chatroom where users can chat globally .',
+        link: 'https://github.com/Ankush-ai/ChatHive',
+    },
+    {
+        iconUrl: car,
+        theme: 'btn-back-blue',
+        name: 'NotesStack',
+        description: 'Designed and built a resonsive web app that les user save/edit/add/delete their digital E-notes.',
+        link: 'https://github.com/Ankush-ai/NotesStack',
+    },
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-pink',
+        name: 'UserFlow',
+        description: 'Built and Developed an  User and Employee Management CRUD web app targetted for MSMEs for thier User/Employee needs.',
+        link: 'https://github.com/Ankush-ai/UserFlow',
+    },
+    {
+        iconUrl: estate,
+        theme: 'btn-back-black',
+        name: 'MarketMoxie',
+        description: 'Developed a web shopping cart application for E-commerce with add to cart feature , facilitating product searches and connecting buyers with sellers.',
+        link: 'https://github.com/Ankush-ai/MarketMoxie',
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-yellow',
+        name: 'AnalizioDash',
+        description: 'App that leverages Data and charts and priovde analytics on an admin dashboard panel.',
+        link: 'https://github.com/Ankush-ai/AnalizioDash',
+    }
+];
